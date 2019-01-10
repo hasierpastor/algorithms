31740b6d4f40eab8a9909569b42fc815c1d71e04
Hasier Pastor <hasier.pastor@gmail.com>
2019-01-09 23:21:40 -0800

    added tree problems

---
+++ trees/maxDepth.js
@@ -0,0 +1,38 @@
+// Given a binary tree, find its maximum depth.
+
+// The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+// Note: A leaf is a node with no children.
+
+// Example:
+
+// Given binary tree [3,9,20,null,null,15,7],
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// return its depth = 3.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+let maxDepth = function(root) {
+  if (!root) return 0;
+  else if (!root.left && !root.right) return 1;
+  else if (!root.left) return maxDepth(root.right) + 1;
+  else if (!root.right) return maxDepth(root.left) + 1;
+  else {
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  }
+};
---
+++ trees/univaluedBinaryTree.js
@@ -0,0 +1,38 @@
+// A binary tree is univalued if every node in the tree has the same value.
+
+// Return true if and only if the given tree is univalued.
+
+// Example 1:
+
+// Input: [1,1,1,1,1,null,1]
+// Output: true
+// Example 2:
+
+// Input: [2,2,2,5,2]
+// Output: false
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+let isUnivalTree = function(root) {
+  let unique = new Set();
+  function dfs(node) {
+    if (node !== null) {
+      dfs(node.left);
+      unique.add(node.val);
+      dfs(node.right);
+    }
+  }
+  dfs(root);
+  console.log(unique);
+  return unique.size === 1;
+};
