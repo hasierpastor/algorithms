257df86a612be9ad3dc9ec4b38deb965664dc3a8
Hasier Pastor <hasier.pastor@gmail.com>
2019-03-15 10:04:10 -0700

    added alternate solution for linked list cycle

--- linked-lists/linkedListCycle.js
+++ linked-lists/linkedListCycle.js
@@ -33,3 +33,18 @@ function hasCycle(head) {
   }
   return false;
 }
+
+function alternateCycle(head) {
+  if (!head || !head.next) return false;
+
+  let slow = head;
+  let fast = head.next;
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow == fast) return true;
+  }
+
+  return false;
+}
