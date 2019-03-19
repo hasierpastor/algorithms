a5b2d2613c1025a323f806e9a483fd961a34bc1a
Hasier Pastor <hasier.pastor@gmail.com>
2019-03-18 20:24:07 -0700

    added plus one problem

---
+++ array-strings/plusOne.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+  let result = new Array(digits.length + 1).fill(0);
+  result[0] = 1;
+  return result;
+};
